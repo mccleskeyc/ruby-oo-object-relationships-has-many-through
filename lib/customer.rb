6a0a03cbbe58bbcb13ca756d4e78f9b92f25eb1d
mccleskeyc <mccleskeyc@gmail.com>
2020-11-24 15:02:18 -0500

    commit tree f0c72b7f93eedaccb9a01e437ac2adce0c0a8edb

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,33 @@
+# ./lib/customer.rb
 class Customer
-  
+    attr_accessor :name, :age # each instance has a name and age
+    # because its an attr_accessor, the customer can change their name or age
+
+    @@all = []
+
+    def initialize(name, age) # name and age set upon initialization
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip=0)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.customer == self
+        end
+    end
+
+    def waiters
+        meals.map do |meal|
+            meal.waiter
+        end
+    end
 end
